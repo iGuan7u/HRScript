c2f82e0b3c675c8b3fa35aabe477fb2acf3a6ffb
sidguan <sidguan@tencent.com>
2019-06-15 21:12:28 +0800

    update script

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import xlrd
 import xlwt
 import os, shutil
 from Staff import Staff
+from SheetHelper import SheetHelper
 
 doneDirPath = os.path.join(os.getcwd(), '文件池', '已完成')
 if not os.path.exists(doneDirPath):
@@ -10,16 +11,18 @@ if not os.path.exists(doneDirPath):
 list = os.listdir('文件池')
 myWorkbook = xlwt.Workbook()
 mySheet = myWorkbook.add_sheet('New Staff')
+
+SheetHelper.writeTheHeader(mySheet)
+
 for i in range(0, len(list)):
     path = os.path.join(os.getcwd(), '文件池', list[i])
     print(path)
 
     if os.path.isfile(path) and os.path.splitext(list[i])[-1] == ".xlsx":
-        print('test')
         book = xlrd.open_workbook(path)
         sh = book.sheet_by_index(0)
         staff = Staff(sh)
-        staff.writeToSheet(mySheet, i)
+        staff.writeToSheet(mySheet, i + 1)
 
         # shutil.move(path, os.path.join(doneDirPath, list[i]))
 
